6cc5a57fbcd1f7d035646e43f0032113cf745d7d
taiyaman <42862764+taiyaman@users.noreply.github.com>
2024-04-30 01:04:16 +0000

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get  'static_pages/about'
-  #root "hello#index"
+  # get 'users/new'
+  # get 'static_pages/home'
+  # get 'static_pages/help'
+  # get  'static_pages/about'
+  # #root "hello#index"
   root "static_pages#home"
+  # get  "static_pages/contact"
+  get  "/help",    to: "static_pages#help"
+  get  "/about",   to: "static_pages#about"
+  get  "/contact", to: "static_pages#contact"
+  get  "/signup",  to: "users#new"
 end
