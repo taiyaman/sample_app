cf57dae0217760e86622b1af6334b4d6b856b067
taiyaman <ubuntu@ip-10-0-15-141.ap-northeast-1.compute.internal>
2024-05-29 12:17:16 +0000

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :microposts, dependent: :destroy
   before_save { self.email = email.downcase }
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -8,6 +9,12 @@ class User < ApplicationRecord
   has_secure_password
   #validates :password, presence: true, length: { minimum: 6 }
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+  
+  # 試作feedの定義
+  # 完全な実装は次章の「ユーザーをフォローする」を参照
+  def feed
+    Micropost.where("user_id = ?", id)
+  end
 
   # 渡された文字列のハッシュ値を返す
   def User.digest(string)
